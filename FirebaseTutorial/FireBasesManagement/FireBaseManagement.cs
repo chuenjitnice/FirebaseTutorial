6a0837dd34e157c7dd2e6b808cac5637c8292cfc
chuenjitnice <101036075+chuenjitnice@users.noreply.github.com>
2022-08-07 17:52:25 +0700

    Fix credential

--- FirebaseTutorial/FireBasesManagement/FireBaseManagement.cs
+++ FirebaseTutorial/FireBasesManagement/FireBaseManagement.cs
@@ -8,7 +8,7 @@ namespace FirebaseTutorial
         readonly IFirebaseClient client;
         readonly IFirebaseConfig config = new FirebaseConfig
         {
-            AuthSecret = "eLaD9VNfw6n4cRnDNYStNSMh8iil7YmqkARmg1sn",
+            AuthSecret = "__<secret_credentail_from_fire_base>__",
             BasePath = "https://csharpworkshop-f8081-default-rtdb.asia-southeast1.firebasedatabase.app/"
         };
         public FireBaseManagement()
